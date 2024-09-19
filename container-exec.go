780ce21f668b956f0b99131c2a40add114601afb
OneThing98 <167092828+OneThing98@users.noreply.github.com>
2024-09-19 18:40:40 +1000

    Update container-exec.go: added more logging

--- container-exec.go
+++ container-exec.go
@@ -25,105 +25,139 @@ var (
 
 // spawns new namespaces and runs the specified containerized process.
 func ContainerExec(container *container.Container) (pid int, err error) {
+	log.Println("Starting ContainerExec")
+
 	if container.NetNsFd > 0 && container.Namespaces.Contains("CLONE_NEWNET") {
+		log.Println("Error: Specified both CLONE_NEWNET and an existing network namespace")
 		return -1, ErrExistingNetworkNamespace
 	}
 
+	log.Println("Resolving root filesystem")
 	rootfs, err := ResolveRootfs(container)
 	if err != nil {
+		log.Printf("Error resolving rootfs: %v", err)
 		return -1, err
 	}
 
+	log.Println("Creating master and console")
 	master, console, err := CreateMasterAndConsole()
 	if err != nil {
+		log.Printf("Error creating master and console: %v", err)
 		return -1, err
 	}
 
+	log.Println("Opening log file for container execution")
 	logger, err := os.OpenFile("/root/logs", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
 	if err != nil {
+		log.Printf("Error opening log file: %v", err)
 		return -1, err
 	}
 	log.SetOutput(logger)
 
+	log.Println("Getting namespace flags and starting the cloning process")
 	flag := getNamespaceFlags(container.Namespaces) | unix.CLONE_VFORK | int(unix.SIGCHLD)
 
 	if pid, err = clone(uintptr(flag)); err != nil {
+		log.Printf("Error cloning process: %v", err)
 		return -1, fmt.Errorf("Error cloning process: %v", err)
 	}
 
 	if pid == 0 {
+		log.Println("Inside child process")
+
+		log.Println("Closing master and standard file descriptors")
 		if err := CloseMasterAndStd(master); err != nil {
 			writeError("close master and std: %v", err)
 		}
+
+		log.Println("Opening terminal")
 		slave, err := OpenTerminal(console, unix.O_RDWR)
 		if err != nil {
-			writeError("open terminal :%v", err)
+			writeError("open terminal: %v", err)
 		}
+
+		log.Println("Duplicating slave to stdout and stderr")
 		if err := DupSlave(slave); err != nil {
 			writeError("dup2 slave: %v", err)
 		}
+
 		if container.NetNsFd > 0 {
+			log.Println("Joining existing network namespace")
 			if err := joinExistingNamespace(container.NetNsFd, "CLONE_NEWNET"); err != nil {
 				writeError("Join existing net namespace: %v", err)
 			}
 		}
+
+		log.Println("Setting session ID")
 		if _, err := setsid(); err != nil {
 			writeError("setsid: %v", err)
 		}
 
+		log.Println("Setting controlling terminal")
 		if err := setctty(); err != nil {
 			writeError("setctty: %v", err)
 		}
 
+		log.Println("Setting parent death signal")
 		if err := parentDeathSignal(); err != nil {
 			writeError("parent death signal: %v", err)
 		}
 
+		log.Println("Setting up new mount namespace")
 		if err := SetUpNewMountNameSpace(rootfs, console, container.ReadonlyFs); err != nil {
 			writeError("setup mount namespace %s", err)
 		}
 
-		//do not include chroot part yet
+		// Skipping chroot part as instructed
 
+		log.Println("Setting container hostname")
 		if err := sethostname(container.ID); err != nil {
 			writeError("sethostname: %v", err)
 		}
 
+		log.Println("Dropping capabilities")
 		if err := capabilities.DropCapabilities(container); err != nil {
 			writeError("drop capabilities: %v", err)
 		}
 
+		log.Println("Setting up user in container")
 		if err := setupUser(container); err != nil {
 			writeError("setup user: %v", err)
 		}
 
 		if container.WorkingDir != "" {
+			log.Printf("Changing to working directory: %s", container.WorkingDir)
 			if err := chdir(container.WorkingDir); err != nil {
 				writeError("chdir to %s: %v", container.WorkingDir, err)
 			}
 		}
 
+		log.Println("Executing container command")
 		if err := exec(container.Command.Args[0], container.Command.Args, container.Command.Env); err != nil {
 			writeError("exec: %v", err)
 		}
-		panic("unreachable")
 
+		log.Println("Reached unreachable code in child process")
+		panic("unreachable")
 	}
 
-	//handle master slave pty communication for the container
+	log.Println("Setting up master-slave pty communication for the container")
 	go func() {
 		if _, err := io.Copy(os.Stdout, master); err != nil {
-			log.Println(err)
+			log.Println("Error copying from master to stdout:", err)
 		}
 	}()
 
 	go func() {
 		if _, err := io.Copy(master, os.Stdin); err != nil {
-			log.Println(err)
+			log.Println("Error copying from stdin to master:", err)
 		}
 	}()
+
+	log.Println("Returning from ContainerExec")
 	return pid, nil
 }
+
 
 // spawns a new command inside an existing container's namespace
 func ContainerExecIn(container *container.Container, cmd *container.Command) (int, error) {
